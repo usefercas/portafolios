9dd9ecf05b77707082df89e63125b84faf68f2d9
usefercas <124436934+usefercas@users.noreply.github.com>
2024-05-30 17:52:31 +0200

    Añadir efecto de máquina de escribir al texto de bienvenida

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import developerHeader from "../assets/header1.png";
 import header from "../assets/header.png";
 import { Link } from "react-scroll";
+import './YourCSSFile.css'; // Asegúrate de importar tu archivo CSS aquí
 
 const Home = () => {
   return (
@@ -15,7 +16,7 @@ const Home = () => {
       />
 
       <div className="absolute md:text-[40px] xl:left-[-20px] xl:text-[40px] md:left-[0] md:w-screen p-6 md:static left-[20%] top-[40%] w-[619px] h-[250px] text-black text-[64px] font-normal">
-        <p translate="no">Hi! I'm Use, a software developer</p>
+        <p className="typewriter-text" translate="no">Hi! I'm Use, a software developer</p>
         <Link
           className="cursor-pointer text-[20px] text-white bg-slate-500 mt-2 p-4"
           to="Projects"
@@ -29,3 +30,4 @@ const Home = () => {
 };
 
 export default Home;
+
