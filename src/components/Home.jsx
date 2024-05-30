26a28d558395fb9e1e2762c222f2994c4fa4db5a
usefercas <124436934+usefercas@users.noreply.github.com>
2024-05-30 19:22:37 +0200

    ajuste text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import './YourCSSFile.css'; // Asegúrate de importar tu archivo CSS aquí
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen overflow-hidden relative" translate="no">
+    <div className="h-screen w-screen overflow-hidden relative">
       <img alt="header" src={header} className="md:hidden w-screen" />
       <div className="md:h-20 min-[700px]:hidden bg-[#2B2B29]"></div>
       <img
@@ -15,19 +15,20 @@ const Home = () => {
         className="absolute left-[50%] top-[350px] md:left-[20%] md:static"
       />
 
-      <div className="absolute md:text-[40px] xl:left-[-20px] xl:text-[40px] md:left-[0] md:w-screen p-6 md:static left-[20%] top-[40%] w-[619px] h-[250px] text-black text-[64px] font-normal">
+      <div className="absolute left-[10%] top-[30%] md:left-[10%] md:top-[25%] p-6 md:static w-[80%] md:w-auto text-black text-[24px] md:text-[32px] xl:text-[40px] font-normal">
         <p className="typewriter-text" translate="no">Hi! I'm Use, a software developer</p>
-        <Link
-          className="cursor-pointer text-[20px] text-white bg-slate-500 mt-2 p-4"
-          to="Projects"
-          smooth="500"
-        >
-          Projects
-        </Link>
+        <div className="mt-4">
+          <Link
+            className="text-[16px] md:text-[20px] text-white bg-slate-500 p-4 inline-block"
+            to="Projects"
+            smooth="500"
+          >
+            Projects
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
-
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Menu from "./Menu";
 import { Link } from "react-scroll";
+
 const NavBar = () => {
-  const [isMenuopen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const links = [
     {
@@ -24,32 +25,34 @@ const NavBar = () => {
   ];
 
   return (
-    <div
-      className={[
-        "absolute",
-        !isMenuopen
-          ? " z-[300] absolute w-[69px] p-10 left-4 h-[47px]"
-          : " items-center justify-around flex flex-col z-[300] fixed w-screen h-screen bg-[#2B2B29]",
-      ]}
-    >
-      <Menu
-        isMenuOpen={isMenuopen}
-        handleMenu={() => setIsMenuOpen(!isMenuopen)}
-      />
+    <html lang="en"> {/* Aquí estableces el idioma en inglés */}
+      <div
+        className={[
+          "absolute",
+          !isMenuOpen
+            ? " z-[300] absolute w-[69px] p-10 left-4 h-[47px]"
+            : " items-center justify-around flex flex-col z-[300] fixed w-screen h-screen bg-[#2B2B29]",
+        ]}
+      >
+        <Menu
+          isMenuOpen={isMenuOpen}
+          handleMenu={() => setIsMenuOpen(!isMenuOpen)}
+        />
 
-      {isMenuopen &&
-        links.map((l) => (
-          <Link
-            className="text-[30px] text-white cursor-pointer"
-            onClick={() => setIsMenuOpen(false)}
-            to={l.link}
-            key={l.id}
-            smooth={500}
-          >
-            {l.link}
-          </Link>
-        ))}
-    </div>
+        {isMenuOpen &&
+          links.map((l) => (
+            <Link
+              className="text-[30px] text-white cursor-pointer"
+              onClick={() => setIsMenuOpen(false)}
+              to={l.link}
+              key={l.id}
+              smooth={500}
+            >
+              {l.link}
+            </Link>
+          ))}
+      </div>
+    </html>
   );
 };
 
